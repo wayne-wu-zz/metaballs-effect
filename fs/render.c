d5364981c326bcb4c4b005fb621b2e201735f8ba
danceformewayne <danceforme_wayne@hotmail.com>
2018-01-18 00:19:34 +0800

    Render blobby effect (unoptimized)
    - Metaball rendering
    - Currently very slow since it's ray marching
    - Need to add bbox and use Ryan's method

--- fs/render.c
+++ fs/render.c
@@ -1,9 +1,11 @@
 #define PARTICLESNUM 2
-#define THRESHOLD 0.01
+#define THRESHOLD 0.40
 #define DT 0.1
-#define STEP 0.01
+#define STEP 0.05
 #define ENTRY 0
 #define EXIT 1
+#define MAXSTEP 400
+#define BBOX_SIZE 2.0
 
 uniform float time;
 uniform vec2 resolution;
@@ -125,90 +127,6 @@ vec4 shade(vec3 eye, vec3 pos, vec3 n, vec4 color)
     return 0.5 * intensity(eye, pos, n, 0.0, 0.0, 1.0) * color;
 }
 
-/*
-// Iterative Merge Sort
-void mergeSort(Intersection[] arr, int n)
-{
-   int curr_size;  // For current size of subarrays to be merged
-                   // curr_size varies from 1 to n/2
-   int left_start; // For picking starting index of left subarray
-                   // to be merged
- 
-   // Merge subarrays in bottom up manner.  First merge subarrays of
-   // size 1 to create sorted subarrays of size 2, then merge subarrays
-   // of size 2 to create sorted subarrays of size 4, and so on.
-   for (curr_size=1; curr_size<=n-1; curr_size = 2*curr_size)
-   {
-       // Pick starting point of different subarrays of current size
-       for (left_start=0; left_start<n-1; left_start += 2*curr_size)
-       {
-           // Find ending point of left subarray. mid+1 is starting 
-           // point of right
-           int mid = left_start + curr_size - 1;
- 
-           int right_end = min(left_start + 2*curr_size - 1, n-1);
- 
-           // Merge Subarrays arr[left_start...mid] & arr[mid+1...right_end]
-           merge(arr, left_start, mid, right_end);
-       }
-   }
-}
-
-*/
-
-/*
-void merge(Intersection arr[], int l, int m, int r)
-{
-    int i, j, k;
-    int n1 = m - l + 1;
-    int n2 =  r - m;
- 
-    // create temp arrays 
-    Intersection L[n1], R[n2];
- 
-    // Copy data to temp arrays L[] and R[]
-    for (i = 0; i < n1; i++)
-        L[i] = arr[l + i];
-    for (j = 0; j < n2; j++)
-        R[j] = arr[m + 1+ j];
- 
-    // Merge the temp arrays back into arr[l..r]
-    i = 0;
-    j = 0;
-    k = l;
-    while (i < n1 && j < n2)
-    {
-        if (L[i].t <= R[j].t)
-        {
-            arr[k] = L[i];
-            i++;
-        }
-        else
-        {
-            arr[k] = R[j];
-            j++;
-        }
-        k++;
-    }
- 
-    // Copy the remaining elements of L[], if there are any
-    while (i < n1)
-    {
-        arr[k] = L[i];
-        i++;
-        k++;
-    }
- 
-    // Copy the remaining elements of R[], if there are any
-    while (j < n2)
-    {
-        arr[k] = R[j];
-        j++;
-        k++;
-    }
-}
-*/
-
 vec3 getNormal(Particle p, vec3 pos)
 {
     return normalize(pos - p.pos);
@@ -216,15 +134,36 @@ vec3 getNormal(Particle p, vec3 pos)
 
 float getFieldValue(Particle p, vec3 pos)
 {
-    vec3 relPos = pos - p.pos;
-    float r = p.radius;
+    //vec3 relPos = pos - p.pos;
+    float r = length(pos - p.pos)/p.radius; // normalize r so that it's from 0 to 1
+    if (r > 1.0)
+        return 0.0; //out of bound
     float g = r * r * r * (r * (r * 6.0 - 15.0) + 10.0);
-    return g;
+    return 1.0 - g; //we want g(0) = 1, g(1) = 0;
+}
+
+float getFieldValue(float R, float d)
+{
+    float r = d/R;
+    if (r > 1.0)
+        return 0.0;
+    float g = r * r * r * (r * (r * 6.0 - 15.0) + 10.0);
+    return 1.0 - g;
 }
 
 float frac(float x)
 {
     return x - floor(x);
+}
+
+bool isOutside(Particle p, vec3 pos)
+{
+    return length(pos - p.pos) > p.radius;
+}
+
+float getDistance(Particle p, vec3 pos)
+{
+    return length(pos - p.pos);
 }
 
 void main()
@@ -247,110 +186,50 @@ void main()
     vec3 imagePos = eye + right * u + up * v + forward * f;
     vec3 dir = normalize(imagePos - eye); // perspective view
 
+    // Temporary hard coded particles
     Particle p[PARTICLESNUM];
-    p[0].pos = vec3(sin(time)+1.0,0.0,0.0);
-    p[0].radius = 1.5;
+    p[0].pos = vec3(sin(time)+0.5,0.0,0.0);
+    p[0].radius = 0.5;
     p[0].color = vec4(1.0,0.0,0.0,0.0); // Red
 
-    p[1].pos = vec3(-sin(time)-1.0,0.0,0.0);
-    p[1].radius = 1.5;
+    p[1].pos = vec3(-sin(time)-0.5,0.0,0.0);
+    p[1].radius = 1.0;
     p[1].color = vec4(0.0,1.0,0.0,0.0); // Green
 
     gl_FragColor = background;
 
-    // Find all the intersections with the particles
-    const int maxNumIntersections = PARTICLESNUM*2;
-    float intersections[maxNumIntersections];
-    Intersection arr[maxNumIntersections];
     float minT = -1.0;
-    Particle minP;
-    for (int i = 0; i < 2; i++)
+
+    float stepSize = -1.0;
+    vec3 pos = imagePos;
+    for (int i = 0; i < MAXSTEP; i++)
     {
-        float t1, t2;
-        bool hitSphere = sphereIntersections(p[i], imagePos, dir, t1, t2);
-        if (hitSphere)
-        {
-            if (minT < 0.0 || min(t1, t2) < minT)
-            {
-                minT = min(t1, t2);
-                minP = p[i];
-            }
-            intersections[i] = t1;
-            intersections[i+1] = t2;
-            //arr[i] = Intersection(t1, i);
-            //arr[i+1] = Intersection(t2, i)
-        }
-        else
-        {
-            // Set them to -1.0 indicating that there's no intersection
-            intersections[i] = -1.0;
-            intersections[i+1] = -1.0;
-            //arr[i] = Intersection(-1.0, i);
-            //arr[i+1] = Intersection(-1.0, i);
-        }
-    }
-
-    if (minT < 0.0)
-    {
-        return; // no intersection
-    }
-
-    /*
-    mergeSort(arr, maxNumIntersections);
-
-    Particle active[PARTICLESNUM];
-    int active_size = 0;
-    for (int i = 0; i < maxNumIntersections; i++)
-    {
-        Intersection tmp = arr[i];
-        if (tmp.type == ENTRY)
-            p[tmp.pIdx].active = true;
-        else if (tmp.type == EXIT)
-            p[tmp.pIdx].active = false;
-
-        float pos = imagePos + dir*tmp.t;
-        while(true)
-        {
-            pos += STEP;
-        }
+        // Evaluate from each particles
+        float val = 0.0;
+        vec3 n;
+        vec4 color;
         for (int j = 0; j < PARTICLESNUM; j++)
+        {   
+            vec3 normal = pos - p[j].pos;
+            float d = length(normal);
+            if (stepSize < 0.0 || d < stepSize)
+                stepSize = d;
+            if (d > p[j].radius)
+                continue;
+            float w = getFieldValue(p[j].radius, d);
+            val += w;
+            n += w*normalize(normal);
+            color += w*p[j].color;
+        }
+        if (val > THRESHOLD)
         {
-            if (p[j].active)
-            {
-                
-            }
+            n /= val;
+            color /= val;
+            gl_FragColor = shade(eye, pos, n, color);
         }
 
+        pos += STEP;        
     }
-    */
 
-    // float totalWeight = 0.0;
-    // vec3 normal;
-    // vec4 color;
-    // vec3 pos;
-    // for (int i = 0; i < maxNumIntersections; i++)
-    // {
-    //     float t = intersections[i];
-    //     if (t < 0.0)
-    //         continue; // no intersection
-
-    //     float dt = t - minT;
-    //     if (dt < DT)
-    //     {
-    //         float w = 1.0 - dt/DT;
-    //         totalWeight += w;
-    //         vec3 hitPos = imagePos + dir*t;
-    //         pos += w*hitPos;
-    //         normal += w*getNormal(p[i/2], hitPos);
-    //         color += w*p[i/2].color;
-    //         //gl_FragColor += w*shadeSphere(eye, imagePos + dir*t, p[i/2]);
-    //     } 
-    // }
-    // pos /= totalWeight;
-    // normal /= totalWeight;
-    // color /= totalWeight;
-    // gl_FragColor = shade(eye, pos, normal, color);
-    
-
-    gl_FragColor = shadeSphere(eye, imagePos + dir*minT, minP);
+    return;
 }
